c5feeda21df6bc3c816697b0a9d6130b6c878ded
hancia <hanna.klimczak@gmail.com>
2019-04-10 22:19:07 +0200

    image preprocessing

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+from matplotlib import pyplot as plt
+from skimage import filters
+
+
+if __name__ == '__main__':
+    filename = 'pics/15_h.jpg'
+    im = cv2.imread(filename)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    imgray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    plt.imshow(im)
+    plt.show()
+
+    cv2.equalizeHist(imgray,imgray)
+    plt.imshow(imgray)
+    plt.show()
+
+    cv2.GaussianBlur(imgray,  (3, 3), 3, imgray)
+    plt.imshow(imgray)
+    plt.show()
+    bwimage = filters.sobel(imgray)
+    for pixel in range (0,bwimage.shape[0]):
+        for i in range (0, bwimage.shape[1]):
+            if bwimage[pixel][i] < 0.03:
+                bwimage[pixel][i] = 255
+            else:
+                bwimage[pixel][i] = 0
+    plt.imshow(bwimage, cmap=plt.get_cmap("gray"))
+    plt.show()
