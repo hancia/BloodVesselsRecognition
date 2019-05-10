68f5c6b1330d978d82272f773b2460220ccc7d97
hancia <hanna.klimczak@gmail.com>
2019-05-10 14:53:22 +0200

    final training and prediction

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import cv2
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Conv2D, Dropout, MaxPooling2D, UpSampling2D, concatenate, core, Flatten, ZeroPadding2D
 from keras_preprocessing.image import img_to_array
-from skimage import filters
 from keras import *
 import numpy as np
-from matplotlib import pyplot as plt
+
+
 # from google.colab import drive
 # drive.mount('/content/drive/')
 
-def get_unet(n_ch, patch_height, patch_width):
-    inputs = Input(shape=(1, patch_height, patch_width))
+
+def get_unet(n):
+    inputs = Input(shape=(1, n, n))
     print(inputs.shape)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first')(inputs)
     print(conv1.shape)
@@ -21,13 +22,13 @@ def get_unet(n_ch, patch_height, patch_width):
     print(conv1.shape)
     pool1 = MaxPooling2D((2, 2))(conv1)
     print(pool1.shape)
-    #
+
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(pool1)
     conv2 = Dropout(0.2)(conv2)
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv2)
     pool2 = MaxPooling2D((2, 2))(conv2)
     print(pool2.shape)
-    #
+
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first')(pool2)
     conv3 = Dropout(0.2)(conv3)
     conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv3)
@@ -40,7 +41,7 @@ def get_unet(n_ch, patch_height, patch_width):
     conv4 = Dropout(0.2)(conv4)
     conv4 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv4)
     print(conv4.shape)
-    #
+
     up2 = UpSampling2D(size=(2, 2))(conv4)
     up2 = concatenate([conv1, up2], axis=1)
     print(up2.shape)
@@ -48,43 +49,31 @@ def get_unet(n_ch, patch_height, patch_width):
     conv5 = Dropout(0.2)(conv5)
     conv5 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv5)
     print(conv5.shape)
-    #
+
     conv6 = Conv2D(2, (1, 1), activation='relu', padding='same', data_format='channels_first')(conv5)
     print(conv6.shape)
     conv6 = core.Reshape((2, 48 * 48))(conv6)
     conv6 = core.Permute((2, 1))(conv6)
-    ############
+
     conv7 = core.Activation('softmax')(conv6)
     print("conv7 shape")
     print(conv7.shape)
 
     model = Model(inputs=inputs, outputs=conv7)
-    model.load_weights("/content/drive/My Drive/iwm2/_last_weights.h5")
 
-    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.3, nesterov=False)
     model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
 
 def build_model(images, masks):
-    # =========== Construct and save the model arcitecture =====
-    n_ch = 1
-    patch_height = 48
-    patch_width = 48
-    model = get_unet(n_ch, patch_height, patch_width)
-
-    # ============  Training ==================================
-    checkpointer = ModelCheckpoint(filepath='./_best_weights.h5', verbose=1, monitor='val_loss', mode='auto',
-                                   save_best_only=True)  # save at each epoch if the validation decreased
+    model = get_unet(48)
 
     model_json = model.to_json()
     with open("/content/drive/My Drive/iwm2/model.json", "w") as json_file:
         json_file.write(model_json)
     model.fit(images, masks, nb_epoch=50, batch_size=32, verbose=2, shuffle=True, validation_split=0.1)
-
-    # ========== Save and test the last model ===================
-    model.save_weights('/content/drive/My Drive/iwm2/_last_weights.h5', overwrite=True)
+    model.save_weights('/content/drive/My Drive/iwm2/weights.h5', overwrite=True)
 
 
 def read_image(path):
@@ -108,64 +97,24 @@ def read_mask(path):
     return im, imgray
 
 
-def adjust_gamma(imgs, gamma=1.0):
-    # build a lookup table mapping the pixel values [0, 255] to
-    # their adjusted gamma values
-    invGamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
-    new_imgs = cv2.LUT(np.array(imgs, dtype = np.uint8), table)
-    # apply gamma correction using the lookup table
-    # new_imgs = np.empty(imgs.shape)
-    # for i in range(imgs.shape[0]):
-    #     new_imgs[i,0] = cv2.LUT(np.array(imgs[i,0], dtype = np.uint8), table)
-    return new_imgs
-
-
-def dataset_normalized(img):
-    img_std = np.std(img)
-    img_mean = np.mean(img)
-    img_normalized = (img-img_mean)/img_std
-    img_normalized = ((img_normalized - np.min(img_normalized)) / (np.max(img_normalized)-np.min(img_normalized)))*255
-    return img_normalized
-
-
-def apply_clahe(img):
+def equalize_histogram(img):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    imgg = clahe.apply(img)
-    # plt.imshow(imgg, cmap=plt.get_cmap("gray"))
-    # plt.show()
+    img = clahe.apply(img)
     return img
 
 
-def standardize(img):
+def normalize(img):
     new_img = np.empty((img.shape[0], img.shape[1]), dtype=float)
     for im in range(0, img.shape[0]):
         for i in range(0, img.shape[1]):
-            new_img[im][i] = float(img[im][i]/255.0)
+            new_img[im][i] = float(img[im][i] / 255.0)
     return new_img
 
 
 def preprocess_image(filename):
     im, imgray = read_image(filename)
-    # cv2.equalizeHist(imgray, imgray)
-    imgray = apply_clahe(imgray)
-    imgray = dataset_normalized(imgray)
-    imgray = adjust_gamma(imgray)
-    imgray = standardize(imgray)
-    # cv2.equalizeHist(imgray, imgray)
-    #
-    # cv2.GaussianBlur(imgray, (3, 3), 3, imgray)
-    # bwimage = imgray
-    # bwimage = filters.sobel(imgray)
-    # for pixel in range(0, bwimage.shape[0]):
-    #     for i in range(0, bwimage.shape[1]):
-    #         if bwimage[pixel][i] < 0.03:
-    #             bwimage[pixel][i] = 0
-    #         else:
-    #             bwimage[pixel][i] = 1
-
-#     plt.imshow(imgray, cmap=plt.get_cmap("gray"))
-#     plt.show()
+    imgray = equalize_histogram(imgray)
+    imgray = normalize(imgray)
     return imgray
 
 
@@ -173,37 +122,26 @@ def extract_patches(img, bwmask):
     p = []
     m = []
     for i in range(10000):
-        x = random.randint(1,img.shape[0]-48)
-        y = random.randint(1,img.shape[1]-48)
+        x = random.randint(1, img.shape[0] - 48)
+        y = random.randint(1, img.shape[1] - 48)
         patch = img[x:x + 48, y:y + 48]
         mask1 = bwmask[x:x + 48, y: y + 48]
-#         if np.sum(np.array(mask1)) <= 0.004:
-#             # plt.imshow(mask1)
-#             # plt.show()
-#             continue
+        msk = np.empty((2304, 2))
         if patch.shape[0] != 48 or patch.shape[1] != 48:
             continue
-        # plt.imshow(patch)
-        # plt.show()
+        pixel = 0
+        for row in mask1:
+            for pix in row:
+                if pix == 0:
+                    msk[pixel][0] = 1
+                    msk[pixel][1] = 0
+                else:
+                    msk[pixel][1] = 1
+                    msk[pixel][0] = 0
+                pixel += 1
         p.append(img_to_array(patch, data_format="channels_first"))
-        m.append(img_to_array(mask1, data_format="channels_first"))
+        m.append(msk)
     return np.array(p), np.array(m)
-
-
-def masks_Unet(masks):
-    im_h = masks.shape[2]
-    im_w = masks.shape[3]
-    masks = np.reshape(masks, (masks.shape[0], im_h * im_w))
-    new_masks = np.empty((masks.shape[0], im_h * im_w, 2))
-    for i in range(masks.shape[0]):
-        for j in range(im_h * im_w):
-            if masks[i, j] == 0:
-                new_masks[i, j, 0] = 1
-                new_masks[i, j, 1] = 0
-            else:
-                new_masks[i, j, 0] = 0
-                new_masks[i, j, 1] = 1
-    return new_masks
 
 
 if __name__ == '__main__':
@@ -225,7 +163,7 @@ if __name__ == '__main__':
     images = np.array(images)
     masks = np.array(masks)
     print(masks.shape)
-    masks = masks_Unet(masks)
+    # masks = masks_Unet(masks)
     print(images.shape)
     print(masks.shape)
     build_model(images, masks)
--- predict.py
+++ predict.py
@@ -6,38 +6,22 @@ import numpy as np
 from keras import *
 from keras.layers import Conv2D, Dropout, MaxPooling2D, UpSampling2D, concatenate, core
 from keras.engine.saving import model_from_json
+from google.colab import drive
+from sklearn.metrics import confusion_matrix
+drive.mount('/content/drive/')
+
+imgNo = 14
 
 
-# from google.colab import drive
-# drive.mount('/content/drive/')
-
-
-def pred_to_imgs(pred, patch_height, patch_width, mode="original"):
-    pred_images = np.empty((pred.shape[0], pred.shape[1]))
-    if mode == "original":
-        for i in range(pred.shape[0]):
-            for pix in range(pred.shape[1]):
-                pred_images[i, pix] = pred[i, pix, 1]
-    elif mode == "threshold":
-        for i in range(pred.shape[0]):
-            for pix in range(pred.shape[1]):
-                if pred[i, pix, 1] > pred[i, pix, 0]:
-                    pred_images[i, pix] = 1
-                else:
-                    pred_images[i, pix] = 0
-    else:
-        pass
-    pred_images = np.reshape(pred_images, (pred_images.shape[0], 1, patch_height, patch_width))
-    return pred_images
-
-
-def get_picture(i):
+def get_picture(i, mask=False):
     if i < 10:
         i = '0{}'.format(i)
-    filename = '/content/drive/My Drive/iwm2/pics/{}_h.jpg'.format(i)
+    if mask == True:
+      filename = '/content/drive/My Drive/iwm2/pics/{}_h.tif'.format(i)
+    else:
+      filename = '/content/drive/My Drive/iwm2/pics/{}_h.jpg'.format(i)
     bwimage = preprocess_image(filename)
     return bwimage
-
 
 def read_image(path):
     print('Reading image ' + path)
@@ -47,81 +31,68 @@ def read_image(path):
     return im, imgray
 
 
-def adjust_gamma(imgs, gamma=1.0):
-    # build a lookup table mapping the pixel values [0, 255] to
-    # their adjusted gamma values
-    invGamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
-    new_imgs = cv2.LUT(np.array(imgs, dtype=np.uint8), table)
-    # apply gamma correction using the lookup table
-    # new_imgs = np.empty(imgs.shape)
-    # for i in range(imgs.shape[0]):
-    #     new_imgs[i,0] = cv2.LUT(np.array(imgs[i,0], dtype = np.uint8), table)
-    return new_imgs
-
-
-def dataset_normalized(img):
-    img_std = np.std(img)
-    img_mean = np.mean(img)
-    img_normalized = (img - img_mean) / img_std
-    img_normalized = ((img_normalized - np.min(img_normalized)) / (
-                np.max(img_normalized) - np.min(img_normalized))) * 255
-    return img_normalized
-
-
-def apply_clahe(img):
+def equalize_histogram(img):
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    imgg = clahe.apply(img)
-    # plt.imshow(imgg, cmap=plt.get_cmap("gray"))
-    # plt.show()
+    img = clahe.apply(img)
     return img
 
 
-def standardize(img):
+def normalize(img):
     new_img = np.empty((img.shape[0], img.shape[1]), dtype=float)
     for im in range(0, img.shape[0]):
         for i in range(0, img.shape[1]):
-            new_img[im][i] = float(img[im][i] / 255.0)
+            new_img[im][i] = float(img[im][i]/255.0)
     return new_img
 
 
 def preprocess_image(filename):
     im, imgray = read_image(filename)
-    # cv2.equalizeHist(imgray, imgray)
-    imgray = apply_clahe(imgray)
-    imgray = dataset_normalized(imgray)
-    imgray = adjust_gamma(imgray)
-    imgray = standardize(imgray)
-    # cv2.equalizeHist(imgray, imgray)
-    #
-    # cv2.GaussianBlur(imgray, (3, 3), 3, imgray)
-    # bwimage = imgray
-    # bwimage = filters.sobel(imgray)
-    # for pixel in range(0, bwimage.shape[0]):
-    #     for i in range(0, bwimage.shape[1]):
-    #         if bwimage[pixel][i] < 0.03:
-    #             bwimage[pixel][i] = 0
-    #         else:
-    #             bwimage[pixel][i] = 1
-    # plt.imshow(imgray, cmap=plt.get_cmap("gray"))
-    # plt.show()
+    imgray = equalize_histogram(imgray)
+    imgray = normalize(imgray)
     return imgray
+
+
+def basic_method(i):
+    if i < 10:
+        i = '0{}'.format(i)
+    filename = '/content/drive/My Drive/iwm2/pics/{}_h.jpg'.format(i)
+    im, imgray = read_image(filename)
+    cv2.equalizeHist(imgray, imgray)
+
+    cv2.GaussianBlur(imgray, (3, 3), 3, imgray)
+    cv2.medianBlur(imgray, 5, imgray)
+    bwimage = imgray
+    bwimage = filters.sobel(imgray)
+    #     print(im)
+    for pixel in range(0, bwimage.shape[0]):
+        for i in range(0, bwimage.shape[1]):
+            if im[pixel][i].all() == 0:
+                bwimage[pixel][i] = 0
+                continue
+            if bwimage[pixel][i] < 0.025:
+                bwimage[pixel][i] = 0
+            else:
+                bwimage[pixel][i] = 1
+    plt.imshow(bwimage, cmap=plt.get_cmap("gray"))
+    plt.show()
+    return bwimage
 
 
 def extract_patches(img):
     p = []
+    i = 0
     for x in range(0, img.shape[0], 48):
         for y in range(0, img.shape[1], 48):
             patch = img[x:x + 48, y:y + 48]
-            # plt.imshow(patch)
-            # plt.show()
             if patch.shape != (48, 48):
-                new_patch = np.ones((48, 48))
+                new_patch = np.empty((48, 48))
                 for x in range(0, patch.shape[0]):
                     for y in range(0, patch.shape[1]):
                         new_patch[x][y] = patch[x][y]
                 patch = new_patch
+            i += 1
             p.append(img_to_array(patch, data_format="channels_first"))
+    print(i)
     return np.array(p)
 
 
@@ -131,16 +102,22 @@ def get_images(img):
     images.extend(patches)
     return np.array(images)
 
+def get_final_patches(pred):
+    patches = np.empty((pred.shape[0], pred.shape[1]))
+    for i in range(pred.shape[0]):
+        for pix in range(pred.shape[1]):
+            patches[i, pix] = pred[i, pix, 1]
+    patches = np.reshape(patches, (patches.shape[0], 1, 48, 48))
+    return patches
 
 def get_partial_image(img, finalpic):
     patches_imgs_test = get_images(img)
     predictions = model.predict(patches_imgs_test, batch_size=32, verbose=2)
 
-    pred_patches = pred_to_imgs(predictions, 48, 48, "original")
-    print(pred_patches.shape)
+    patches = get_final_patches(predictions)
     x = 0
     y = 0
-    for pic in pred_patches:
+    for pic in patches:
         for p in pic:
             temp = finalpic[x:x + 48, y:y + 48]
             finalpic[x:x + 48, y:y + 48] = p[0:0 + temp.shape[0], 0:0 + temp.shape[1]]
@@ -151,15 +128,24 @@ def get_partial_image(img, finalpic):
     return finalpic
 
 
+def calculate_metrics(img1, img2):
+    tn, fp, fn, tp = confusion_matrix(img1, img2).ravel()
+    acc = (tn + tp) / (tn + fp + fn + tp)
+    print("Accuracy {}".format(acc))
+    sen = tp / (tp + fn)
+    print("Sensitivity {}".format(sen))
+    spec = tn / (tn + fp)
+    print("Specificity {}".format(spec))
+
+
+img = get_picture(imgNo)
+basic_img = basic_method(imgNo)
 json_file = open('/content/drive/My Drive/iwm2/model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
-# load weights into new model
-model.load_weights("/content/drive/My Drive/iwm2/_last_weights.h5")
-print("Loaded model from disk")
+model.load_weights("/content/drive/My Drive/iwm2/weights.h5")
 
-img = get_picture(10)
 finalpic = np.empty((img.shape[0], img.shape[1]))
 
 finalpic = get_partial_image(img, finalpic)
@@ -168,5 +154,12 @@ print("Printing final picture")
 plt.imshow(finalpic, cmap=plt.get_cmap("gray"))
 plt.show()
 
+ground_truth = get_picture(imgNo, mask=True)
+calculate_metrics(ground_truth.astype(int).flatten(), basic_img.astype(int).flatten())
+calculate_metrics(ground_truth.astype(int).flatten(), finalpic.astype(int).flatten())
+
 finalpic *= 255
-cv2.imwrite("/content/drive/My Drive/iwm2/results/finalimage.jpg", finalpic)
+cv2.imwrite("/content/drive/My Drive/iwm2/results/{}_nn.jpg".format(imgNo), finalpic)
+
+basic_img *= 255
+cv2.imwrite("/content/drive/My Drive/iwm2/results/{}_basic.jpg".format(imgNo), basic_img)
